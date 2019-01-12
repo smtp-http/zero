6679e1e076a093f82f664f3bf617af9c2b2a2be4
kevinzu <zukeqiang@foxmail.com>
2019-01-12 16:42:20 +0800

    service_test update

--- service_test.go
+++ service_test.go
@@ -5,15 +5,24 @@ import (
 	"net"
 	"testing"
 	"time"
+	"container/list"
 )
+
+var SessionList *list.List
 
 func TestService(t *testing.T) {
 	host := "127.0.0.1:18787"
+
+	SessionList = list.New()
+
+	fmt.Println(SessionList)
 
 	ss, err := NewSocketService(host)
 	if err != nil {
 		return
 	}
+
+	
 
 	// ss.SetHeartBeat(5*time.Second, 30*time.Second)
 
@@ -23,7 +32,7 @@ func TestService(t *testing.T) {
 
 	go NewClientConnect()
 
-	timer := time.NewTimer(time.Second * 1)
+	timer := time.NewTimer(time.Second * 1000)
 	go func() {
 		<-timer.C
 		ss.Stop("stop service")
@@ -32,11 +41,24 @@ func TestService(t *testing.T) {
 
 	t.Log("service running on " + host)
 	ss.Serv()
+
 }
 
 func HandleMessage(s *Session, msg *Message) {
 	fmt.Println("receive msgID:", msg)
 	fmt.Println("receive data:", string(msg.GetData()))
+	for e := SessionList.Front(); e != nil; e = e.Next() {
+		//fmt.Print(e.Value) //输出list的值,01234
+		session := e.Value.(*Session)
+		fmt.Println("++++++++++++++++++++++ 2")
+		fmt.Println(session)
+		fmt.Println("---------------------- 2")
+		conn := session.GetConn()
+		str := "shit ===== shit ++++"
+		var data []byte = []byte(str)
+		msg := NewMessage(12,data)
+		conn.SendMessage(msg)
+	}
 }
 
 func HandleDisconnect(s *Session, err error) {
@@ -44,7 +66,11 @@ func HandleDisconnect(s *Session, err error) {
 }
 
 func HandleConnect(s *Session) {
-	fmt.Println(s.GetConn().GetName() + " connected.")
+	fmt.Println("++++++++++++++++++++++ 1")
+	fmt.Println(s)
+	fmt.Println("---------------------- 1")
+	fmt.Println(s.GetConn().GetName() + " connected." )
+	SessionList.PushBack(s)
 }
 
 func NewClientConnect() {
@@ -65,4 +91,7 @@ func NewClientConnect() {
 		return
 	}
 	conn.Write(data)
+
+	
+
 }
